0707a3677b164b7afb66a06a774df2d8ff084996
GabeWohlwend <gabewohlwend@siu.edu>
2017-03-29 16:29:53 -0500

    Added CalendarView to calendar activity
    Added CalendarView to calendar activity

--- ScholarsProgramApp/app/src/main/java/siucs/scholarsprogramapp/CalendarActivity.java
+++ ScholarsProgramApp/app/src/main/java/siucs/scholarsprogramapp/CalendarActivity.java
@@ -2,12 +2,34 @@ package siucs.scholarsprogramapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CalendarActivity extends AppCompatActivity {
+import java.security.acl.AclEntry;
+
+public class CalendarActivity extends AppCompatActivity
+{
+
+    CalendarView calendarView;
+    TextView dateDisplay;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        dateDisplay = (TextView) findViewById(R.id.dateDisplay);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
+        {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth)
+            {
+                dateDisplay.setText("Date: " + month + " / " + dayOfMonth + " / " + year);
+            }
+        });
     }
 }
